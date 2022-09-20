16793b1494240b8fea28dc8d84eb320042c83f92
Dean Forth <d.forth@samsung.com>
2022-09-20 10:51:11 -0700

    trigger action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  // remove comment
   return (
     <div>
       <h1>Hi there!</h1>
